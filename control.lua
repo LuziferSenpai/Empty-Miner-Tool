ce2f6cf18374d73d2329236c48d9da9520ad3602
Luzifer <mrcookiesmedia@gmail.com>
2018-12-26 05:05:36 +0100

    Release 0.2.0

--- control.lua
+++ control.lua
@@ -10,31 +10,36 @@ script.on_event( { de.on_player_selected_area, de.on_player_alt_selected_area },
 	for _, e in pairs( event.entities ) do
 		if e.valid then
 		 	if e.type == "mining-drill" and not e.mining_target then
+		 		local d = e.drop_position
+		 		local e1 = s.find_entities_filtered{ area = { { d.x + 0.1, d.y + 0.1 }, d }, type = { "container", "logistic-container" } }
+		 		if e1[1] ~= nil then
+		 			local e1 = e1[1]
+		 			local b = e1.bounding_box
+		 			local tl = b.left_top
+		 			local br = b.right_bottom
+		 			local t = true
+		 			tl.x = tl.x - 1
+		 			tl.y = tl.y - 1
+		 			br.x = br.x + 1
+		 			br.y = br.y + 1
+		 			local e2 = s.find_entities_filtered{ area = { tl, br }, type = "mining-drill" }
+		 			if e2[1] ~= nil then
+		 				for _, e3 in pairs( e2 ) do
+		 					if e3.mining_target then
+		 						local d1 = e3.drop_position
+		 						local e4 = s.find_entities_filtered{ area = { { d.x + 0.1, d.y + 0.1 }, d }, type = { "container", "logistic-container" } }
+		 						if e4[1] ~= nil and e4[1] == e1 then
+		 							t = false
+		 							break
+		 						end
+		 					end
+		 				end
+		 			end
+		 			if t then
+		 				e1[1].order_deconstruction( f, p )
+		 			end
+		 		end
 				e.order_deconstruction( f, p )
-			elseif ( e.type == "container" or e.type == "logistic-container" ) then
-				local n = e.name
-				local b = e.bounding_box
-				local tl = b.left_top
-				local br = b.right_bottom
-				local target = true
-
-				tl.x = tl.x - 1
-				tl.y = tl.y - 1
-				br.x = br.x + 1
-				br.y = br.y + 1
-				
-				local e1 = s.find_entities_filtered{ area = { tl, br }, type = "mining-drill" }
-
-				if #e1 > 0 then
-					for _, e2 in pairs( e1 ) do
-						if s.find_entity( n, e2.drop_position ) and e2.mining_target then
-							target = false
-						end
-					end
-				end
-				if target then
-					e.order_deconstruction( f, p )
-				end
 			end
 		end
 	end
