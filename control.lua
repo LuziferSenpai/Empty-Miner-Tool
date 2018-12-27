3b5a4886fbd154f66dcac975f2b0c39f5305a9c8
Luzifer <mrcookiesmedia@gmail.com>
2018-12-27 17:28:03 +0100

    Version 0.2.1

--- control.lua
+++ control.lua
@@ -11,7 +11,7 @@ script.on_event( { de.on_player_selected_area, de.on_player_alt_selected_area },
 		if e.valid then
 		 	if e.type == "mining-drill" and not e.mining_target then
 		 		local d = e.drop_position
-		 		local e1 = s.find_entities_filtered{ area = { { d.x + 0.1, d.y + 0.1 }, d }, type = { "container", "logistic-container" } }
+		 		local e1 = s.find_entities_filtered{ position = d, type = { "container", "logistic-container" } }
 		 		if e1[1] ~= nil then
 		 			local e1 = e1[1]
 		 			local b = e1.bounding_box
@@ -24,11 +24,11 @@ script.on_event( { de.on_player_selected_area, de.on_player_alt_selected_area },
 		 			br.y = br.y + 1
 		 			local e2 = s.find_entities_filtered{ area = { tl, br }, type = "mining-drill" }
 		 			if e2[1] ~= nil then
-		 				for _, e3 in pairs( e2 ) do
+		 				for i, e3 in pairs( e2 ) do
 		 					if e3.mining_target then
 		 						local d1 = e3.drop_position
-		 						local e4 = s.find_entities_filtered{ area = { { d.x + 0.1, d.y + 0.1 }, d }, type = { "container", "logistic-container" } }
-		 						if e4[1] ~= nil and e4[1] == e1 then
+		 						local e4 = s.find_entities_filtered{ position = d, type = { "container", "logistic-container" } }
+		 						if e4[1] ~= nil and e4[1].position == e1.position then
 		 							t = false
 		 							break
 		 						end
@@ -36,7 +36,7 @@ script.on_event( { de.on_player_selected_area, de.on_player_alt_selected_area },
 		 				end
 		 			end
 		 			if t then
-		 				e1[1].order_deconstruction( f, p )
+		 				e1.order_deconstruction( f, p )
 		 			end
 		 		end
 				e.order_deconstruction( f, p )
